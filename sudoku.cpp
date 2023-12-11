924ee9d32b6e7d1f0227c1b76400ad3b19056ef7
TomasVavra <tomvavra1@gmail.com>
2023-12-12 00:33:50 +0100

    7 commit

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,21 +1,22 @@
 # include <iostream>
 
 int s[9][9][10];
-int i,j,k,l,m,n,p;
+
 const int z[9][9] = {
-    {0,0,0,0,0,0,3,9,6},
-    {3,0,0,0,0,8,2,0,0},
-    {0,0,0,1,0,6,4,0,8},
-    {5,9,0,0,0,1,0,0,0},
-    {0,1,0,5,2,7,0,8,0},
-    {0,0,0,4,0,0,0,3,1},
-    {1,0,4,7,0,2,0,0,0},
-    {0,0,7,8,0,0,0,0,3},
-    {9,2,8,0,0,0,0,0,0}
+{9,0,0,0,6,3,0,5,0},
+{0,0,2,0,0,1,0,9,0},
+{0,0,4,9,0,0,7,0,1},
+{0,0,6,0,1,0,0,8,0},
+{0,0,0,3,0,6,0,0,0},
+{0,1,0,0,8,0,9,0,0},
+{7,0,9,0,0,5,8,0,0},
+{0,4,0,8,0,0,2,0,0},
+{0,3,0,1,4,0,0,0,9}
 };
 
 void read ()
 {
+    int i,j,k;
     for (i=0; i<9; i++) 
     {
         for (j=0; j<9; j++) 
@@ -39,6 +40,7 @@ void read ()
 
 void write (int st[9][9][10])
 {
+    int i,j,k;
     for (i=0; i<9; i++) 
     {
         for (j=0; j<9; j++) 
@@ -59,6 +61,7 @@ void write (int st[9][9][10])
 
 void rows_columns_square (int st[9][9][10])
 {
+    int i,j,l,m,n;
     for (i=0; i<9; i++) 
     {
         for (j=0; j<9; j++) 
@@ -83,9 +86,9 @@ void rows_columns_square (int st[9][9][10])
     }
 }
 
-void check (int st[9][9][10])       // check if there is only one solution for s[i][j][0]
+void check_third_dimension (int st[9][9][10])       // check if there is only one solution for s[i][j][0]
 {
-    int o = 0;
+    int i,j,k,o;
     int winner;
     for (i=0; i<9; i++) 
     {
@@ -108,6 +111,33 @@ void check (int st[9][9][10])       // check if there is only one solution for s
     }
 }
 
+void check_rows_columns_square (int st[9][9][10])       // check if the number is present in row, column, square
+{                                                   // dodelat
+    int i,j,l,m,n;
+    for (i=0; i<9; i++) 
+    {
+        for (j=0; j<9; j++) 
+        {
+            if (s[i][j][0] != 0)
+            {
+                for (l=0; l<9; l++)
+                {
+                    s[l][j][s[i][j][0]] = 0;     // delete the number in j column
+                    s[i][l][s[i][j][0]] = 0;     // delete the number in i row
+                }
+
+                for (m=0; m<3; m++)
+                {
+                    for (n=0; n<3; n++)
+                    {
+                        s[(i/3)*3+m][(j/3)*3+n][s[i][j][0]] = 0;    // delete the number in square
+                    }
+                }
+            }
+        }
+    }
+}
+
 int main () {
 
 read ();
@@ -116,16 +146,16 @@ write (s);
 rows_columns_square (s);
 write (s);
 
-check (s);
+check_third_dimension (s);
 write (s);
 
 std::cout << "**********************" << std::endl;
 std::cout << std::endl;
 
-for (p=0; p<10; p++)
+for (int p=0; p<100; p++)
 {
     rows_columns_square (s);
-    check (s);
+    check_third_dimension (s);
     write (s);
 }
 
