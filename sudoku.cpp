b27b29faa90bc9d7f129c26bb32918d66f8ad4f1
TomasVavra <tomvavra1@gmail.com>
2023-12-11 22:57:32 +0100

    5 commit

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,7 +1,7 @@
 # include <iostream>
 
 int s[9][9][10];
-int i,j,k,l;
+int i,j,k,l,m,n;
 const int z[9][9] = {
     {0,0,0,0,0,0,3,9,6},
     {3,0,0,0,0,8,2,0,0},
@@ -41,14 +41,16 @@ void write (int st[9][9][10])
                 std::cout << s[i][j][k];
                 if (k==0) {std::cout << "*";}
             }
-            std::cout << "  ";
+            std::cout << " ";
+            if (j==2 || j==5) {std::cout << "  ";}
         }
         std::cout << std::endl;
+        if (i==2 || i==5) {std::cout << std::endl;}
     }
-    std::cout << std::endl << std::endl;
+    std::cout << std::endl << std::endl << std::endl;
 }
 
-void columns (int st[9][9][10])
+void rows_columns_square (int st[9][9][10])
 {
     for (i=0; i<9; i++) 
     {
@@ -58,7 +60,16 @@ void columns (int st[9][9][10])
             {
                 for (l=0; l<9; l++)
                 {
-                    s[i][l][s[i][j][0]] = 0;
+                    s[l][j][s[i][j][0]] = 0;     // delete the number in j column
+                    s[i][l][s[i][j][0]] = 0;     // delete the number in i row
+                }
+
+                for (m=0; m<3; m++)
+                {
+                    for (n=0; n<3; n++)
+                    {
+                        s[(i/3)*3+m][(j/3)*3+n][s[i][j][0]] = 0;    // delete the number in square
+                    }
                 }
             }
         }
@@ -70,7 +81,7 @@ int main () {
 read ();
 write (s);
 
-columns (s);
+rows_columns (s);
 write (s);
 
 return 0;
