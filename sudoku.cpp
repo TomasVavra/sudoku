4a0ab06736b07ed78eb78efc869dff6e32380b15
TomasVavra <tomvavra1@gmail.com>
2023-12-13 00:02:59 +0100

    10 commit

--- sudoku.cpp
+++ sudoku.cpp
@@ -101,6 +101,10 @@ void check_third_dimension (int st[9][9][10])       // check if there is only on
                     o++;
                     winner = s[i][j][k];
                 }
+                if (s[i][j][0] != 0)
+                {
+                    s[i][j][k] = 0;
+                }
             }
             if (o==1)
             {
@@ -112,28 +116,43 @@ void check_third_dimension (int st[9][9][10])       // check if there is only on
 
 void check_rows_columns_square (int st[9][9][10])       // check if the number is present in row, column, square
 {                                                   // dodelat
-    int i,j,k,l,m,n,o,winner;
+    int i,j,k,l,m,n,n_in_row,n_in_column,n_in_square;
     for (i=0; i<9; i++) 
     {
         for (j=0; j<9; j++) 
         {
             for (k=1; k<10; k++) 
             {
-                if ((s[i][j][k] != 0) && (s[i][j][0]==0))
+                if ((s[i][j][k] != 0) && (s[i][j][0] == 0))
                 {
-                    o=0;
+                    n_in_row = 0;
+                    n_in_column = 0;
+                    n_in_square = 0;
                     for (l=0; l<9; l++)
                     {
-                        if (s[i][l][k] ==0)
+                        if (s[i][l][k] !=0)
                         {
-                            o++;
-                            winner = s[i][j][k];
+                            n_in_row++;
                         }
-                        
+                        if (s[l][j][k] !=0)
+                        {
+                            n_in_column++;
+                        }                     
                     }
-                    if (o==1)
+                    for (m=0; m<3; m++)
                     {
-                        s[i][j][0] = winner;
+                        for (n=0; n<3; n++)
+                        {
+                            if (s[(i/3)*3+m][(j/3)*3+n][k] != 0)
+                            {
+                                n_in_square++;
+                            }
+                        }
+                    }
+
+                    if (n_in_row == 1 || n_in_column == 1 || n_in_square == 1 )
+                    {
+                        s[i][j][0] = s[i][j][k];
                     }
                 }
             }
@@ -147,21 +166,18 @@ int main () {
 read ();
 write (s);
 
-rows_columns_square (s);
-write (s);
-
-check_third_dimension (s);
-write (s);
-
 std::cout << "**********************" << std::endl;
 std::cout << std::endl;
 
-for (int p=0; p<100; p++)
+for (int p=0; p<20; p++)
 {
     rows_columns_square (s);
+    check_rows_columns_square (s);
     check_third_dimension (s);
     write (s);
 }
+
+
 
 
 return 0;
