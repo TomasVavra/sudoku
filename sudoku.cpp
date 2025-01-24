72e9c7f96ea1211faa6f72c1910c0b7a0cdba292
Tomas Vavra <tomvavra1@gmail.com>
2025-01-24 19:52:22 +0100

    check_rows cols squares not working together

--- sudoku.cpp
+++ sudoku.cpp
@@ -4,36 +4,41 @@
 
 int main () {
 
-
-// const int instructions[9][9] = {
-// {9,0,0,0,6,3,0,5,0},
-// {0,0,2,0,0,1,0,9,0},
-// {0,0,4,9,0,0,7,0,1},
-// {0,0,6,0,1,0,0,8,0},
-// {0,0,0,3,0,6,0,0,0},
-// {0,1,0,0,8,0,9,0,0},
-// {7,0,9,0,0,5,8,0,0},
-// {0,4,0,8,0,0,2,0,0},
-// {0,3,0,1,4,0,0,0,9}
-// };
-
+// instruction 1
 const int instructions[9][9] = {
-{0,1,0,6,0,0,9,0,0},
-{0,0,0,0,3,0,0,0,1},
-{9,0,8,1,7,4,5,0,0},
-{0,0,1,8,0,0,0,7,4},
-{0,0,0,0,0,0,0,0,0},
-{5,6,0,0,0,7,1,0,0},
-{0,0,9,7,4,6,8,0,5},
-{6,0,0,0,9,0,0,0,0},
-{0,0,7,0,0,2,0,3,0},
+{9,0,0,0,6,3,0,5,0},
+{0,0,2,0,0,1,0,9,0},
+{0,0,4,9,0,0,7,0,1},
+{0,0,6,0,1,0,0,8,0},
+{0,0,0,3,0,6,0,0,0},
+{0,1,0,0,8,0,9,0,0},
+{7,0,9,0,0,5,8,0,0},
+{0,4,0,8,0,0,2,0,0},
+{0,3,0,1,4,0,0,0,9}
 };
+
+// instruction 2
+// const int instructions[9][9] = {
+// {0,1,0,6,0,0,9,0,0},
+// {0,0,0,0,3,0,0,0,1},
+// {9,0,8,1,7,4,5,0,0},
+// {0,0,1,8,0,0,0,7,4},
+// {0,0,0,0,0,0,0,0,0},
+// {5,6,0,0,0,7,1,0,0},
+// {0,0,9,7,4,6,8,0,5},
+// {6,0,0,0,9,0,0,0,0},
+// {0,0,7,0,0,2,0,3,0},
+// };
 
 int solution[9][9][10];
 int last_solution[9][9][10];
 
 read (instructions, solution);
 write (solution);
+
+// because instruction 1 has more solutions?
+//solution[0][1][0] = 7;
+
 
 std::cout << "**********************" << std::endl;
 std::cout << std::endl;
@@ -48,11 +53,17 @@ while (! are_solutions_identical(solution, last_solution))
     // only_once_in_rows_columns_square (solution);
     // delete_obsolete_possibilities (solution);
     
-    // check_rows (solution);
-    // delete_obsolete_possibilities (solution);
+    check_rows (solution);
+    delete_obsolete_possibilities (solution);
     
-    // check_cols (solution);
-    // delete_obsolete_possibilities (solution);
+    delete_possibilities_in_row_col_square (solution);
+    delete_obsolete_possibilities (solution);
+
+    check_cols (solution);
+    delete_obsolete_possibilities (solution);
+
+    delete_possibilities_in_row_col_square (solution);
+    delete_obsolete_possibilities (solution);
 
     check_squares (solution);
     delete_obsolete_possibilities (solution);
@@ -63,6 +74,7 @@ while (! are_solutions_identical(solution, last_solution))
     write (solution);
     
 }
+
 
 std::cout << "is solution valid: " << is_solution_valid (solution) << std::endl;
 
